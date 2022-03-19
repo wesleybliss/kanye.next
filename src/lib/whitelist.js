c82a1d88810a8dcf61b7117cd8567b9f7c5d61c8
Wesley Bliss <wesley.bliss@gmail.com>
2022-03-19 03:24:57 -0500

    Added CORS whitelist

---
+++ src/lib/whitelist.js
@@ -0,0 +1,14 @@
+
+// @todo This is a very inefficient way to manage this list
+export default [
+    'foo'
+].map(it => new RegExp(it))
+/* export default [
+    'http://localhost(.*)',
+    'http://127.0.0.1(.*)',
+    'https://([a-z0-9]+[.])*wesleybliss[.]com',
+    // https://kanye-says-9ouz8eb0i-wesleybliss.vercel.app
+    'https://(.*)[-]wesleybliss[.]vercel[.]app',
+    'http://kanye-says.local(.*)',
+    'https://kanye-says[.]vercel[.]app',
+].map(it => new RegExp(it)) */
--- src/pages/api/yeet.js
+++ src/pages/api/yeet.js
@@ -1,11 +1,35 @@
 import Cors from 'cors'
 import initMiddleware from '../../lib/init-middleware'
+import whitelist from '../../lib/whitelist'
 import { getRandomQuote } from '../../lib/db'
 
+const corsOptions = {
+    
+    methods: ['GET', 'OPTIONS'],
+    
+    origin: (origin, callback) => {
+        
+        // Optionally restrict origins here
+        origin && console.log('CORS', origin)
+        
+        // @todo not sure why this is undefined sometimes with GET requests
+        if (typeof origin === 'undefined')
+            return callback(null, true)
+        
+        if (whitelist.some(it => it.test(origin)))
+            callback(null, true)
+        else
+            console.log('origin', origin, 'did not match\n', whitelist.join('\n')) || callback(new Error('Unauthorized'))
+        
+    }
+    
+}
+
 const cors = initMiddleware(
-    Cors({
+    /* Cors({
         methods: ['GET', 'OPTIONS'],
-    })
+    }) */
+    Cors(corsOptions)
 )
 
 const handler = async (req, res) => {
